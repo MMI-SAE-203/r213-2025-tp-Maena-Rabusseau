a3d896e432de9e126a852420e8cc0aa0e3c969d8
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2025-01-31 11:18:04 +0100

    1er commit changement des données statiques à dynamiques

---
+++ src/backend.mjs
@@ -0,0 +1,69 @@
+/* Maena Rabusseau Groupe A1*/
+
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function AllMaisons() {
+    let records = await pb.collection('Maison').getFullList();
+    console.log("ici udef ?", records);
+    records = records.map((Maison) => {
+        Maison.img = pb.files.getURL(Maison, Maison.images);
+        return Maison;
+    });
+
+    return records;
+}
+
+/*Etape 11 : Ecrire la fonction ”oneID” qui retourne une seule maison en passant l’id de cette maison en ´
+param`etre et tester la
+export async function oneId(id) {
+    const Onerecords = await pb.collection('Maison').getOne(id);
+    return Onerecords;
+}
+    */
+
+/* Etape 12: ´ Ecrire la fonction ”allMaisonsFavori” qui retourne toutes les maisons en favori et tester la. 
+
+export async function allMaisonsFavori() {
+    const records = await pb.collection('Maison').getFullList({ filter: 'favori = true', });
+    return records;
+}
+
+*/
+
+/* Etape 13 : Ecrire la fonction ”allMaisonsSorted” qui retourne toutes les maisons ordonn´ees par ordre crois- ´
+sant de leur prix et tester la.
+
+
+export async function allMaisonsSorted() {
+    const records = await pb.collection('Maison').getFullList({ sort: 'prix', });
+    return records;
+}
+*/
+
+/* Etape 14 : Ecrire une fonction ”bySurface” qui prend en param`etre une surface et qui retourne la liste de ´
+toutes les maisons ayant une superficie sup´erieur `a surface.
+
+export async function bySurface(s) {
+    const records = await pb.collection('Maison').getFullList({ filter: `surface > ${s}`, });
+    return records;
+}
+*/
+/* Etape 15 : Ecrire une fonction ”surfaceORprice” qui prend en param`etre une surface et un prix (p) et qui ´
+retourne la liste de toutes les maisons ayant une superficie sup´erieur `a surface ou un prix inf´erieur `a p
+
+export async function surfaceORprice(p, s) {
+    const records = await pb.collection('Maison').getFullList({ filter: `prix > ${p} && surface > ${s}`, });
+
+    return records;
+}
+*/
+
+
+
+
+/* a faire avec le prof 
+Etape 18. ´ Modifier la collection maison pour ajouter la relation avec la collection agent.
+Etape 19. ´ Ajouter une fonction dans le fichier backend.mjs permettant de remonter les donn´ees d’un agent
+en passant son id en param`etre.
+*/
